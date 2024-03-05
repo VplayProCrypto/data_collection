10d29be66f8865bf13642dc14060e59efec5b93c
Anmol Goyal <anmol.1310@yahoo.co.in>
2024-03-05 05:44:59 +0530

    add changes

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 from typing import Optional
 from sqlalchemy import ForeignKey
 from sqlalchemy import String
@@ -6,13 +6,15 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import JSONB
+from datetime import datetime
 
 class Base(DeclarativeBase):
     pass
 
 class Collection(Base):
     __tablename__ = "collection"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    # id: Mapped[int] = mapped_column(primary_key=True)
     opensea_slug: Mapped[str] = mapped_column(unique=True)
     name: Mapped[str] = mapped_column(String(50))
     description: Mapped[str]
@@ -40,17 +42,46 @@ class Collection(Base):
 
 class Contract(Base):
     __tablename__ = "contract"
-    collection_id: Mapped[str] = mapped_column(ForeignKey("collection.id"))
+    collection_slu: Mapped[str] = mapped_column(ForeignKey("collection.collection"))
     contract_address: Mapped[str] = mapped_column(unique = True)
     chain: Mapped[str]
     def __repr__(self) -> str:
         return f"Contract(collection={self.collection_id!r}, contract_address={self.contract_address!r}, chain={self.chain!r})"
 
+class Fee(Base):
+    __tablename__ = "fee"
+    collection_slug: Mapped[str] = mapped_column(ForeignKey("collection.opensea_slug"))
+    fee: Mapped[float]
+    recipient: Mapped[str]
+
 class Nft(Base):
     __tablename__ = "nft"
-    collection_id: Mapped[str] = mapped_column(ForeignKey("collection.id"))
+    opensea_slug: Mapped[str] = mapped_column(ForeignKey("collection.opensea_slug"))
     token_id: Mapped[str]
     contract_address: Mapped[str]
+    token_standard: Mapped[str]
+    name: Mapped[str]
+    descp: Mapped[str]
+    image_url: Mapped[str]
+    metadata_url: Mapped[str]
+    opensea_url: Mapped[str]
+    updated_at: Mapped[datetime]
+    traits: Mapped[JSONB]
+    is_nsfw: Mapped[bool]
+    is_disabled: Mapped[bool]
     
     def __repr__(self) -> str:
         return f"Nft(id={self.id!r}, email_address={self.chain!r})"
+
+class NftSale(Base):
+    token_id: Mapped[str]
+    contract_address: Mapped[str]
+    buyer: Mapped[str]
+    selle: Mapped[str]
+    token_id: Mapped[str]
+    token_id: Mapped[str]
+    token_id: Mapped[str]
+    token_id: Mapped[str]
+    token_id: Mapped[str]
+def get_metadata():
+    return Base.metadata
---
+++ db/postgres_injector_orm.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+from sqlalchemy import text, func
+from sqlalchemy.dialects.postgresql import insert
+from models
