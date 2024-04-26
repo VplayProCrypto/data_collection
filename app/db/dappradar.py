31bc64fa9d7813442067be24bfced7316b54f973
tanay-sgl <tanay.sagrolikar@gmail.com>
2024-04-26 16:22:15 +1000

    extract_uaw

--- app/db/dappradar.py
+++ app/db/dappradar.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-def get_uaw_from_dappradar(dappradar_id: str, time_range: str):
+def get_uaw_from_dappradar(dappradar_id: str, time_range: str) -> str:
     base_url = os.environ.get('DAPPRADAR_BASE_URL')
     api_key = os.environ.get('DAPPRADAR_API_KEY')
     
@@ -12,4 +12,10 @@ def get_uaw_from_dappradar(dappradar_id: str, time_range: str):
                                 "x-api-key": api_key
                             })
     response.raise_for_status()
-    return response.json()
+    data = response.json()
+    
+    if data["success"]:
+        uaw = data["results"]["metrics"]["uaw"]
+        return str(uaw)
+    else:
+        return "Failed to retrieve UAW data."
