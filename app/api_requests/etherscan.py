d61246d946bec35cb50cc604f93a370533b56276
tanay-sgl <tanay.sagrolikar@gmail.com>
2024-05-09 12:03:31 +1000

    refactored_mapper

--- app/api_requests/etherscan.py
+++ app/api_requests/etherscan.py
@@ -5,41 +5,65 @@ from pprint import pprint
 import argparse
 import time
 from datetime import datetime, date, timedelta
-# from utils import append_data_to_file
+from orm.models import ERC20Transfer
 
 
 class EtherScan:
-    def __init__(self ):
+    def __init__(self):
 
-        self.api_key = os.environ.get('ETHERSCAN_API_KEY')
-        self.base_url = os.environ.get('ETHERSCAN_BASE_URL')
+        self.api_key = os.environ.get("ETHERSCAN_API_KEY")
+        self.base_url = os.environ.get("ETHERSCAN_BASE_URL")
         self.headers = {
-            'accept': 'application/json',
+            "accept": "application/json",
         }
-    
+
     def get_block_from_timestamp(self, timestamp: int):
         params = {
-            'module': 'block',
-            'timestamp': str(timestamp),
-            'action': 'getblocknobytime',
-            'closest': 'before',
-            'apiKey': self.api_key
+            "module": "block",
+            "timestamp": str(timestamp),
+            "action": "getblocknobytime",
+            "closest": "before",
+            "apiKey": self.api_key,
         }
 
-        r = requests.get(self.base_url, params = params).json()
-        return r['result']
-    
-    def get_erc20_transfers(self, contract_address: str, after_date: datetime):
-        assert(isinstance(contract_address, str))
+        r = requests.get(self.base_url, params=params).json()
+        return r["result"]
+
+    def get_erc20_transfers(
+        self, contract_address: str, after_date: datetime, collection_slug: str
+    ):
+        assert isinstance(
+            contract_address, str
+        ), "Please provide a valid contract address"
+
         block_num = self.get_block_from_timestamp(int(after_date.timestamp()))
+
         params = {
-            'action': 'tokentx',
-            'module': 'account',
-            'contractaddress': contract_address,
-            'sort': 'asc',
-            'apikey': self.api_key,
-            'startblock': str(block_num)
+            "action": "tokentx",
+            "module": "account",
+            "contractaddress": contract_address,
+            "sort": "asc",
+            "apikey": self.api_key,
+            "startblock": str(block_num),
         }
 
-        r = requests.get(self.base_url, headers = self.headers, params = params).json()
-        return r['result']
+        response = requests.get(
+            self.base_url, headers=self.headers, params=params
+        ).json()
+
+        transfers = [
+            ERC20Transfer(
+                buyer=transfer_data["to"],
+                seller=transfer_data["from"],
+                contract_address=transfer_data["contractAddress"],
+                price=float(transfer_data["value"]),
+                symbol=transfer_data["tokenSymbol"],
+                decimals=int(transfer_data["tokenDecimal"]),
+                transaction_hash=transfer_data["hash"],
+                event_timestamp=datetime.fromtimestamp(int(transfer_data["timeStamp"])),
+                collection_slug=collection_slug,
+            )
+            for transfer_data in response["result"]
+        ]
+
+        return transfers
