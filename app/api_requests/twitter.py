65e3f42e123c2d7de22677c9a811a60440e4c81f
tanay-sgl <tanay.sagrolikar@gmail.com>
2024-05-13 11:59:07 +1000

    tweepy

--- app/api_requests/twitter.py
+++ app/api_requests/twitter.py
@@ -5,46 +5,16 @@ import tweepy
 
 
 def get_user_public_metrics(username):
-    bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
+    client = tweepy.Client(
+        consumer_key=os.environ.get("TWITTER_API_KEY"),
+        consumer_secret=os.environ.get("TWITTER_API_SECRET"),
+        access_token=os.environ.get("TWITTER_ACCESS_TOKEN"),
+        access_token_secret=os.environ.get("TWITTER_ACCESS_SECRET"),
+    )
 
-    if not bearer_token:
-        raise ValueError("Bearer token not found in environment variables")
+    response = client.get_user(username)
 
-    headers = {"Authorization": f"Bearer {bearer_token}"}
-
-    params = {"user.fields": "public_metrics"}
-
-    url = f"https://api.twitter.com/2/users/by/username/{username}"
-
-    response = requests.get(url, headers=headers, params=params)
-    print("Response Headers:", response.headers)
-    print("Response Body:", response.text)
-    if response.status_code == 200:
-        data = response.json()
-        if "data" in data:
-            public_metrics = data["data"]["public_metrics"]
-            return public_metrics
-        else:
-            raise ValueError(f"User '{username}' not found")
+    if response.data:
+        return response.data
     else:
-        raise Exception(f"Request failed with status code {response.status_code}")
-
-
-def test_tweepy(username):
-
-    client = tweepy.Client(bearer_token=os.environ.get("TWITTER_BEARER_TOKEN"))
-
-    # Get Users
-
-    # This endpoint/method returns a variety of information about one or more users
-    # specified by the requested IDs or usernames
-
-    user_ids = [2244994945, 6253282]
-
-    # By default, only the ID, name, and username fields of each user will be
-    # returned
-    # Additional fields can be retrieved using the user_fields parameter
-    response = client.get_users(ids=user_ids, user_fields=["public_metrics"])
-
-    for user in response.data:
-        print(user.username, user.public_metrics)
+        return None
