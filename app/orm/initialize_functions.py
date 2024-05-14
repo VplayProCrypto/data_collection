a991e38ee8464a33d3093ed4b3758b99d00cad36
tanay-sgl <tanay.sagrolikar@gmail.com>
2024-05-14 15:39:45 +1000

    collection_dynamic

--- app/orm/initialize_functions.py
+++ app/orm/initialize_functions.py
@@ -23,10 +23,17 @@ def rank_social(site: str, followers: int) -> float:
     return 0.0
 
 
-def add_game(collection_slug: str, num_pages: int) -> None:
-    opensea = OpenSea()
-    collection_response = opensea.get_collection(collection_slug)
-    nft_response = opensea.get_nfts_for_collection(collection_slug, num_pages)
+# if no data exists mark followers as -1
+# for now since we can't get social data we need to manually input it when updating
+def add_collection_dynamic(
+    collection_slug: str,
+    twitter_followers=-1,
+    facebook_followers=-1,
+    instagram_followers=-1,
+    reddit_users=-1,
+    discord_users=-1,
+    telegram_supergroup_size=-1,
+) -> None:
     collection_stats = opensea.get_collection_stats(collection_slug)
     collection_dynamic = CollectionDynamic(
         game_id=collection_stats["game_id"],
@@ -43,22 +50,48 @@ def add_game(collection_slug: str, num_pages: int) -> None:
         average_price=collection_response["average_price"],
         monthly_uaw=get_uaw_from_dappradar("9495", "30d"),
         daily_uaw=get_uaw_from_dappradar("9495", "24h"),
+        discord_users=discord_users,
         discord_sentiment=rank_social(
             "discord",
-            get_guild_member_count(collection_response["collection"]["discord_url"]),
+            discord_users,
+            # get_guild_member_count(collection_response["collection"]["discord_url"]),
         ),
+        twitter_followers=twitter_followers,
         twitter_sentiment=rank_social(
             "twitter",
-            get_user_public_metrics(collection_response["collection"]["twitter_url"])[
-                "followers"
-            ],
+            twitter_followers,
+            # get_user_public_metrics(collection_response["collection"]["twitter_url"])["followers"],
         ),
+        telegram_supergroup_size=telegram_supergroup_size,
         telegram_sentiment=rank_social(
             "telegram",
-            get_supergroup_member_count(
-                collection_response["collection"]["telegram_url"]
-            ),
+            telegram_supergroup_size,
+            # get_supergroup_member_count(collection_response["collection"]["telegram_url"]),
         ),
-        # TODO add others later
+        facebook_followers=facebook_followers,
+        facebook_sentiment=rank_social(
+            "facebook",
+            facebook_followers,
+            # get_user_public_metrics(collection_response["collection"]["facebook_url"])["followers"],
+        ),
+        instagram_followers=instagram_followers,
+        instagram_sentiment=rank_social(
+            "instagram",
+            instagram_followers,
+            # get_user_public_metrics(collection_response["collection"]["instagram_url"])["followers"],
+        ),
+        reddit_users=reddit_users,
+        reddit_sentiment=rank_social(
+            "reddit",
+            reddit_users,
+            # get_user_public_metrics(collection_response["collection"]["reddit_url"])["followers"],
+        ),
     )
+
+
+def add_game(collection_slug: str, num_pages: int) -> None:
+    opensea = OpenSea()
+    collection_response = opensea.get_collection(collection_slug)
+    nft_response = opensea.get_nfts_for_collection(collection_slug, num_pages)
+
     # TODO NFT Listings, Events, -> get corresponding NFTs
