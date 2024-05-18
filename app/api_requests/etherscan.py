24c6a207a5fbff131e80737571d3180447dc7728
tanay-sgl <tanay.sagrolikar@gmail.com>
2024-05-18 12:58:16 +1000

    save_transfers

--- app/api_requests/etherscan.py
+++ app/api_requests/etherscan.py
@@ -6,6 +6,7 @@ import argparse
 import time
 from datetime import datetime, date, timedelta
 from orm.models import ERC20Transfer
+from sqlmodel import create_engine, Session
 
 
 class EtherScan:
@@ -67,3 +68,48 @@ class EtherScan:
         ]
 
         return transfers
+
+    def save_erc20_transfers(
+        self,
+        db: Session,
+        contract_address: str,
+        after_date: datetime,
+        collection_slug: str,
+    ):
+        assert isinstance(
+            contract_address, str
+        ), "Please provide a valid contract address"
+
+        block_num = self.get_block_from_timestamp(int(after_date.timestamp()))
+
+        params = {
+            "action": "tokentx",
+            "module": "account",
+            "contractaddress": contract_address,
+            "sort": "asc",
+            "apikey": self.api_key,
+            "startblock": str(block_num),
+        }
+
+        response = requests.get(
+            self.base_url, headers=self.headers, params=params
+        ).json()
+
+        for transfer_data in response["result"]:
+            erc20transfer = ERC20Transfer(
+                buyer=transfer_data["to"],
+                seller=transfer_data["from"],
+                contract_address=transfer_data["contractAddress"],
+                price=float(transfer_data["value"]),
+                symbol=transfer_data["tokenSymbol"],
+                decimals=int(transfer_data["tokenDecimal"]),
+                transaction_hash=transfer_data["hash"],
+                event_timestamp=datetime.fromtimestamp(int(transfer_data["timeStamp"])),
+                collection_slug=collection_slug,
+            )
+
+            db.add(erc20transfer)
+            db.commit()
+
+        print("Added ERC20 transfers")
+        db.close()
