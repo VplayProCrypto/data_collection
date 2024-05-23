7ab10675edfd3e38731c622d769bc9d87692444c
tanay-sgl <tanay.sagrolikar@gmail.com>
2024-05-23 14:06:01 +1000

    save_all_Erc20_transfers

--- app/api_requests/etherscan.py
+++ app/api_requests/etherscan.py
@@ -7,8 +7,8 @@ import time
 from datetime import datetime, date, timedelta
 from sqlmodel import create_engine, Session
 from sqlalchemy.engine import Engine
-from ..orm.models import ERC20Transfer
-from .. import keys
+from orm.models import ERC20Transfer
+from sqlalchemy.exc import IntegrityError
 
 
 class EtherScan:
@@ -16,8 +16,8 @@ class EtherScan:
 
         self.api_key = os.environ.get("ETHERSCAN_API_KEY")
         # self.base_url = os.environ.get("ETHERSCAN_BASE_URL")
-        self.api_key = keys.etherscan_api_key
-        self.base_url = 'https://api.etherscan.io/api/'
+        # self.api_key = keys.etherscan_api_key
+        self.base_url = "https://api.etherscan.io/api/"
         self.headers = {
             "accept": "application/json",
         }
@@ -34,9 +34,7 @@ class EtherScan:
         r = requests.get(self.base_url, params=params).json()
         return r["result"]
 
-    def get_erc20_transfers(
-        self, contract_address: str, after_date: datetime
-    ):
+    def get_erc20_transfers(self, contract_address: str, after_date: datetime):
         assert isinstance(
             contract_address, str
         ), "Please provide a valid contract address"
@@ -75,8 +73,8 @@ class EtherScan:
         #     for transfer_data in response["result"]
         # ]
 
-        return response['result']
-    
+        return response["result"]
+
     def get_erc20_transfers_new(
         self, contract_address: str, after_date: datetime, collection_slug: str
     ):
@@ -143,18 +141,87 @@ class EtherScan:
         ).json()
 
         for transfer_data in response["result"]:
-            db.add(ERC20Transfer(
-                buyer=transfer_data["to"],
-                seller=transfer_data["from"],
-                contract_address=transfer_data["contractAddress"],
-                price=float(transfer_data["value"]),
-                symbol=transfer_data["tokenSymbol"],
-                decimals=int(transfer_data["tokenDecimal"]),
-                transaction_hash=transfer_data["hash"],
-                event_timestamp=datetime.fromtimestamp(int(transfer_data["timeStamp"])),
-                collection_slug=collection_slug,
-            ))
+            db.add(
+                ERC20Transfer(
+                    buyer=transfer_data["to"],
+                    seller=transfer_data["from"],
+                    contract_address=transfer_data["contractAddress"],
+                    price=float(transfer_data["value"]),
+                    symbol=transfer_data["tokenSymbol"],
+                    decimals=int(transfer_data["tokenDecimal"]),
+                    transaction_hash=transfer_data["hash"],
+                    event_timestamp=datetime.fromtimestamp(
+                        int(transfer_data["timeStamp"])
+                    ),
+                    collection_slug=collection_slug,
+                )
+            )
         db.commit()
 
         print("Added ERC20 transfers")
+        db.close()
+
+    def save_all_erc20_transfers(
+        self,
+        db: Session,
+        contract_address: str,
+        after_date: datetime,
+        collection_slug: str,
+    ):
+        assert isinstance(
+            contract_address, str
+        ), "Please provide a valid contract address"
+
+        block_num = self.get_block_from_timestamp(int(after_date.timestamp()))
+        page = 1
+
+        while True:
+            params = {
+                "action": "tokentx",
+                "module": "account",
+                "contractaddress": contract_address,
+                "sort": "asc",
+                "apikey": self.api_key,
+                "startblock": str(block_num),
+                "page": str(page),
+                "offset": "100",
+            }
+
+            response = requests.get(
+                self.base_url, headers=self.headers, params=params
+            ).json()
+
+            if not response["result"]:
+                break
+
+            for transfer_data in response["result"]:
+                transaction_hash = transfer_data["hash"]
+                event_timestamp = datetime.fromtimestamp(
+                    int(transfer_data["timeStamp"])
+                )
+
+                try:
+                    erc20_transfer = ERC20Transfer(
+                        buyer=transfer_data["to"],
+                        seller=transfer_data["from"],
+                        contract_address=transfer_data["contractAddress"],
+                        price=float(transfer_data["value"]),
+                        symbol=transfer_data["tokenSymbol"],
+                        decimals=int(transfer_data["tokenDecimal"]),
+                        transaction_hash=transaction_hash,
+                        event_timestamp=event_timestamp,
+                        collection_slug=collection_slug,
+                    )
+                    db.add(erc20_transfer)
+                    db.commit()
+                except IntegrityError:
+                    db.rollback()
+                    print(
+                        f"Error inserting Transaction Hash: {transaction_hash}, Event Timestamp: {event_timestamp}\n"
+                    )
+
+            db.commit()
+            print(f"Added ERC20 transfers for page {page}")
+            page += 1
+
         db.close()
