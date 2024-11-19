f5bf06e3113f419f0b58fbc1d77026f0e061866e
Anmol Goyal <anmol.1310@yahoo.co.in>
2024-11-19 15:23:37 +0530

    Fixed skip router bug

--- app/dependencies.py
+++ app/dependencies.py
@@ -14,7 +14,7 @@ sessionDeps = Annotated[Session, Depends(get_session)]
 
 class filterQueryParamsBase(BaseModel):
     limit: int = Field(default=10, le=100, gt=0)
-    skip: int = Field(default=0, gt=0)
+    skip: int = Field(default=0, ge=0)
 
 class filterQueryParams(filterQueryParamsBase):
     tags: str = Field(default=None)
